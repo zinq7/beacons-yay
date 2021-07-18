c9db3f314035e0b1c3e79caed92ab74eafab733a
zinq7 <87585065+zinq7@users.noreply.github.com>
2021-07-18 16:58:19 -0400

    trying to add wasp to factory pool

--- scripts/units/uefAir.js
+++ scripts/units/uefAir.js
@@ -1,1 +1,8 @@
+
+
+const t1wasp = extendContent(UnitType, "t1wasp", {
+});
+t1wasp.constructor = () => extend(UnitEntity, {
+});
+
 Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(t1wasp, 60 * 20, ItemStack.with(Items.metaglass, 30, Items.titanium, 20)));
