02e0e96e226236d6a158fc2bd774c9cb1db4dbab
zinq7 <87585065+zinq7@users.noreply.github.com>
2021-07-23 12:33:12 -0400

    removed spare files

--- scripts/units/shkAir.js
+++ scripts/units/shkAir.js
@@ -1,7 +1,6 @@
 //Don't ask where I stol-copied this code from.
 /* "shkAir" Unit path
 */
-//const register = require("work-pls/units/unitReg");
 const shkAirAI = prov(() => {
   var u = extend(FlyingAI, {
     updateMovement(){
@@ -15,64 +14,6 @@ const shkAirT1 = extendContent(UnitType, "strike", {
 shkAirT1.constructor = () => extend(UnitEntity, {
 });
 //register(shkAirT1);
-const shkBoltTrail = Effect(15, e => {
-	Draw.color(Color.valueOf("#ffff00"), Color.valueOf("#ffffff"), e.fin());
-	Drawf.tri(e.x, e.y, 7 * e.fout(), 10 + 15 * (1 - e.finpow()), e.rotation);
-	Drawf.tri(e.x, e.y, 7 * e.fout(), 10 + 15 * (1 - e.finpow()), e.rotation + 180);
-    Lines.stroke(e.fout() * 2);
-    const de = new Floatc2({get(x, y){
-    Lines.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fout() * 6);
-    }}) 
-    Angles.randLenVectors(e.id, 2, 7 * e.fout(), e.rotation, 30 * e.fout(),de);
-});
-const shkBoltDesp = Effect(20, e => {
-	Draw.color(Color.valueOf("#ffffff"), Color.valueOf("#ffff00"), e.fin());
-	Fill.circle(e.x, e.y, e.fout() * 6);
-    Lines.stroke(e.fout() * 3);
-    Lines.circle(e.x, e.y, e.finpow() * 15);
-});
-const shkBoltHit = Effect(15, e => {
-	Draw.color(Color.valueOf("#ffffff"), Color.valueOf("#ffff00"), e.fin());
-    const ctdf = new Floatc2({get(x, y){
-		Drawf.tri(e.x + x, e.y + y, 3 * e.fout(), 15 * e.fout(), Mathf.angle(x, y));
-		Drawf.tri(e.x + x, e.y + y, 3 * e.fout(), 3 * e.fout(), Mathf.angle(x, y) + 180);
-    }}) 
-    Angles.randLenVectors(e.id, 3, 28 * e.fin(), e.rotation, 30, ctdf);
-    const vyum = new Floatc2({get(x, y){
-		Fill.circle(e.x + x, e.y + y, e.fout() * 2);
-    }}) 
-    Angles.randLenVectors(e.id + 1, 4, 28 * e.fin(), e.rotation, 30, vyum);
-});
-const shkBoltShoot = Effect(15, e => {
-	Draw.color(Color.valueOf("#ffffff"), Color.valueOf("#ffff00"), e.fin());
-	Fill.circle(e.x, e.y, e.fout() * (8 - Math.abs(8 - (e.fin() * 16))));
-    Lines.stroke(e.fout() * 2);
-    Lines.circle(e.x, e.y, e.finpow() * 7);
-    const ctdf = new Floatc2({get(x, y){
-		Drawf.tri(e.x + x, e.y + y, 3 * e.fout(), 15 * e.fout(), Mathf.angle(x, y));
-		Drawf.tri(e.x + x, e.y + y, 3 * e.fout(), 3 * e.fout(), Mathf.angle(x, y) + 180);
-    }}) 
-    Angles.randLenVectors(e.id, 3, 28 * e.fin(), e.rotation, 60, ctdf);
-    const vyum = new Floatc2({get(x, y){
-		Fill.circle(e.x + x, e.y + y, e.fout() * 2);
-    }}) 
-    Angles.randLenVectors(e.id + 1, 4, 28 * e.fin(), e.rotation + 180, 60 * e.fout(), vyum);
-});
-const shkStrikeBolt = extend(PointBulletType, {});
-shkStrikeBolt.damage = 25;
-shkStrikeBolt.speed = 240;
-shkStrikeBolt.lifetime = 1;
-shkStrikeBolt.trailSpacing = 10;
-shkStrikeBolt.trailEffect = shkBoltTrail;
-shkStrikeBolt.tileDamageMultiplier = 0.6;
-shkStrikeBolt.smokeEffect = Fx.hitFuse;
-shkStrikeBolt.shootEffect = shkBoltShoot;
-shkStrikeBolt.hitEffect = shkBoltHit;
-shkStrikeBolt.hitSound = loadSound("strikeZap");
-shkStrikeBolt.despawnEffect = shkBoltDesp;
-shkStrikeBolt.status = StatusEffects.shocked;
-shkStrikeBolt.statusDuration = 30;
-
 shkAirT1.ammoType = AmmoTypes.powerHigh;
 
 Blocks.airFactory.plans.add(new UnitFactory.UnitPlan(shkAirT1, 60 * 20, ItemStack.with(Items.silicon, 10, Items.lead, 25, Items.copper, 15)));
