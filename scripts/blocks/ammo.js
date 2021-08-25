819e5e98bda11b949c6a429a301b3aedac85bdbd
zinq7 <87585065+zinq7@users.noreply.github.com>
2021-08-25 17:06:03 -0400

    try 5

--- scripts/blocks/ammo.js
+++ scripts/blocks/ammo.js
@@ -1,20 +1,10 @@
-
+Events.on(ContentInitEvent, () => {
 //available items and liquids
 const nukeItem = Vars.content.getByName(ContentType.item, "beacons-yay-nuke");
 const bulletItem = Vars.content.getByName(ContentType.item, "beacons-yay-bullet");
 const zincItem = Vars.content.getByName(ContentType.item, "beacons-yay-zinq");
 const zinqItem = Vars.content.getByName(ContentType.item, "beacons-yay-zinq-good");
 const jungleJuiceLiquid = Vars.content.getByName(ContentType.item, "beacons-yay-jungle-juice");
-//salvo with Bullet
-const bulletSalvo = extend(BasicBulletType, {});
-bulletSalvo.height = 12;
-bulletSalvo.width = 14;
-bulletSalvo.lifetime = 60;
-bulletSalvo.speed = 4;
-bulletSalvo.ammoMultiplier = 6;
-bulletSalvo.damage = 20;
-bulletSalvo.frontColor = "d3d3d3";
-bulletSalvo.backColor = "d3d3d3";
 
 //duo with Nuke
 const standardNuke = extend(BasicBulletType, {});
@@ -30,7 +20,8 @@ standardNuke.frontColor = "d3d3d3";
 standardNuke.backColor = "d3d3d3";
 
 //testing
-const nukeduo = Seq.with(nukeItem,standardNuke).toArray(ammoTypes);
+const nukeduo = ObjectMap.with(nukeItem,standardNuke).toArray(ammoTypes);
 //regestering the ammos
 Blocks.duo.ammoTypes.addAll(nukeduo);
-Blocks.salvo.ammoTypes.addAll(bulletItem,bulletSalvo);
+Blocks.duo.init();
+});
