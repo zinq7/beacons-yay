f5fd8e937ddcb1b3c673242a9cf03cb9fbae7c23
zinq7 <87585065+zinq7@users.noreply.github.com>
2022-05-15 17:03:25 -0400

    Fixing collisions with tanks (and removing empty ammo)

--- scripts/blocks/ammo.js
+++ scripts/blocks/ammo.js
@@ -1,30 +1,17 @@
-/*Events.on(ContentInitEvent, () => {
+/*
+this is some javascript code in order to attempt to override the DUO with a new ammo (nuke)
+at the moment it's not working in the slightest, i'm focusing my efforts on other things for now
+
+
+Events.on(ContentInitEvent, () => {
     //available items and liquids
     const nukeItem = Vars.content.getByName(ContentType.item, "beacons-yay-nuke");
     const bulletItem = Vars.content.getByName(ContentType.item, "beacons-yay-bullet");
     const zincItem = Vars.content.getByName(ContentType.item, "beacons-yay-zinq");
     const zinqItem = Vars.content.getByName(ContentType.item, "beacons-yay-zinq-good");
     const jungleJuiceLiquid = Vars.content.getByName(ContentType.item, "beacons-yay-jungle-juice");
+});
 
-    //duo with Nuke
-    const standardNuke = extend(BasicBulletType, {});
-    standardNuke.height = 12;
-    standardNuke.width = 14;
-    standardNuke.lifetime = 60;
-    standardNuke.speed = 4;
-    standardNuke.ammoMultiplier = 10;
-    standardNuke.damage = 20;
-    standardNuke.splashDamage = 200;
-    standardNuke.splashDamageRadius = 20;
-    standardNuke.frontColor = "d3d3d3";
-    standardNuke.backColor = "d3d3d3";
-
-    //testing
-    //regestering the ammos
-    Blocks.duo.ammo.add(nukeItem, standardNuke);
-    Blocks.duo.ammo.add(Items.metaglass, standardNuke);
-    Blocks.duo.init();
-});*/
 Events.on(WorldLoadEvent, e => {
     extend(Block, "duo", {
         init() {
@@ -43,6 +30,7 @@ Events.on(WorldLoadEvent, e => {
             standardNuke.backColor = "d3d3d3";
             const nukeItem = Vars.content.getByName(ContentType.item, "beacons-yay-nuke");
             Blocks.duo.ammo.add(nukeItem, standardNuke)
+            Blocks.duo.init();
         }
     });
-})
+}); */
